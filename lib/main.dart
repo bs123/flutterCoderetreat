ac42fd630265b8efa6a81e3cf7a7b8df12f3e33b
javadev <nospam@nospam.nospam>
2019-11-16 13:08:44 +0100

    germanword

--- lib/main.dart
+++ lib/main.dart
@@ -3,18 +3,10 @@
 // found in the LICENSE file.
 // https://www.youtube.com/watch?v=V1nU25DLc2E
 
+import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:german_words/german_words.dart';
-
 import 'package:english_words/english_words.dart';
-
-//import 'dart:math';
-//
-//int _random;
-//int get random => _random;
-//set random(int ran) => _random = ran;
-//
-//int _random2 = Random().nextInt(germanWords.length);
 
 void main() => runApp(MyApp());
 
@@ -30,26 +22,27 @@ class MyApp extends StatelessWidget {
 
 class RandomWordsState extends State<RandomWords> {
   final _suggestions = <WordPair>[];
-  final _biggerFont = const TextStyle(fontSize: 18.0);
+  final _biggerFont = const TextStyle(
+    fontSize: 33.0,
+    fontWeight: FontWeight.w900,
+  );
 
-//  TextStyle bold24Roboto = TextStyle(
-//    color: Colors.white,
-//    fontSize: 24,
-//    fontWeight: FontWeight.w900,
-//  );
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Startup Name Generator'),
+        title: Text('Kinder'),
       ),
       body: _buildSuggestions(),
     );
   }
 
   Widget _buildRow(WordPair pair) {
+    int _random = Random().nextInt(germanWords.length);
+    random(int ran) => _random = ran;
     return ListTile(
       title: Text(
+        // germanWords.length.toString(),
         pair.asPascalCase,
         style: _biggerFont,
       ),
@@ -59,13 +52,11 @@ class RandomWordsState extends State<RandomWords> {
   Widget _buildSuggestions() {
     return ListView.builder(
         padding: const EdgeInsets.all(16.0),
-        itemBuilder: /*1*/ (context, i) {
+        itemBuilder: (context, i) {
           if (i.isOdd) return Divider();
-          /*2*/
-
-          final index = i ~/ 2; /*3*/
+          final index = i ~/ 2;
           if (index >= _suggestions.length) {
-            _suggestions.addAll(generateWordPairs().take(10)); /*4*/
+            _suggestions.addAll(generateWordPairs().take(10));
           }
           return _buildRow(_suggestions[index]);
         });
